a175a3fc593e05a1a003fbd89fc1e3e3c40e21f2
sobhika11 <sobhika1105@gmail.com>
2025-12-19 09:10:22 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const env = require('dotenv');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+// enviourment variables
+env.config();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// routes
+const userRoutes = require('./routes/user.routes');
+const testRoutes = require('./routes/test.routes');
+
+app.use('/public', express.static(path.join(__dirname, "uploads")));
+app.use('/api', userRoutes)
+app.use('/api', testRoutes)
+
+// mongodb connection
+const connectDB = (dburl) => {
+    return mongoose.connect(dburl)
+        .then(() => console.log('Database Connected'))
+        .catch((err) => console.error('DB connection failed:', err));
+};
+
+const start = async () => {
+    try {
+        await connectDB(process.env.dburl);
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
