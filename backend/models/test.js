bd7cdef646df84d90fdbfd225640e7060f51918b
sobhika11 <sobhika1105@gmail.com>
2025-12-19 22:53:21 +0530

    Added test model schema

---
+++ backend/models/test.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const testSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    test_name: {
+      type: String,
+      required: true
+    },
+
+    test_link_by_user: {
+      type: String
+    },
+
+    test_code: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    start_time: {
+      type: Date,
+      required: true
+    },
+
+    end_time: {
+      type: Date,
+      required: true
+    },
+
+    no_of_candidates_appear: {
+      type: Number,
+      default: 0
+    }
+);
+
+module.exports = mongoose.model("Test", testSchema);
