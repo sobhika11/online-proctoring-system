7fefe2ed8c4c3473821a25e1d22f7c7f77d90a5b
sobhika11 <sobhika1105@gmail.com>
2025-12-19 23:04:00 +0530

    added test and user reference to test attempts

--- backend/models/testAttempt.js
+++ backend/models/testAttempt.js
@@ -1,6 +1,17 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const testAttemptSchema = new Schema(
-  {},
+  {
+    testId: {
+      type: Schema.Types.ObjectId,
+      ref: "Test",
+      required: true
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
   { timestamps: true }
 );
