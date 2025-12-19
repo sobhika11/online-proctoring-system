8186b554f0e735aef4ccd9a596f4aff987c3452d
sobhika11 <sobhika1105@gmail.com>
2025-12-19 16:14:17 +0530

    installed all packages

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,7 @@
 const express = require('express');
-const env = require('dotenv');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 
-
-env.config();
-app.use(express.json());
-
-const userRoutes = require('./routes/user.routes');
-const testRoutes = require('./routes/test.routes');
-
-app.use('/public', express.static(path.join(__dirname, "uploads")));
-app.use('/api', userRoutes)
-app.use('/api', testRoutes)
-
-// mongodb connection
-const connectDB = (dburl) => {
-    return mongoose.connect(dburl)
-        .then(() => console.log('Database Connected'))
-        .catch((err) => console.error('DB connection failed:', err));
-};
-
-const start = async () => {
-    try {
-        await connectDB(process.env.dburl);
-        app.listen(process.env.PORT, () => {
-            console.log(`Server started is running on port ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-start();
+require("dotenv").config();
+console.log(process.env.MONGO_URI);
