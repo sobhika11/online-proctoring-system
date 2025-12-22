e5b835b34c8bfd67f2904c683be2ee20a1355b4d
sobhika11 <sobhika1105@gmail.com>
2025-12-22 07:45:30 +0530

    fixed bug

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -15,45 +15,35 @@ const {
 
 const authRole = require("../middlewares/authRole");
 
-
-
-const {
-  createTest,
-  userCreatedTests,
-  testRegister,
-  testAdminData
-} = require("../controllers/test.controller");
-
-const {
-  validateCreateTest,
-  validateTestCodeParam
-} = require("../middlewares/validate");
+// Main part -Admin creates test
 router.post(
   "/create",
   authRole(["admin"]),
   validateCreateTest,
   createTest
 );
+
+// This part for Admin views candidates of a test
 router.get(
   "/admin/:test_code",
   authRole(["admin"]),
   validateTestCodeParam,
   testAdminData
 );
+
+// This is for Student registers for test
 router.post(
   "/register/:test_code",
   authRole(["student"]),
   validateTestCodeParam,
   testRegister
 );
+
+// This is for Admin views tests created by them
 router.get(
   "/my-tests",
   authRole(["admin"]),
   userCreatedTests
 );
-router.post("/create", validateCreateTest, createTest);
-router.get("/my-tests", userCreatedTests);
-router.post("/register/:test_code", validateTestCodeParam, testRegister);
-router.get("/admin/:test_code", validateTestCodeParam, testAdminData);
 
 module.exports = router;
