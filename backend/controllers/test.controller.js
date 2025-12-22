c71ac17d1bcd78d3a06e561558dbcc1d8492ba9a
sobhika11 <sobhika1105@gmail.com>
2025-12-22 07:47:20 +0530

    Function that handles after user creates test

--- backend/controllers/test.controller.js
+++ backend/controllers/test.controller.js
@@ -7,3 +7,10 @@ exports.createTest = async (req, res) => {
     return res.status(500).json({ error: err.message });
   }
 };
+exports.userCreatedTests = async (req, res) => {
+  try {
+    return res.status(200).json({ message: "userCreatedTests controller" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
