b1482de01341dad652d6fa09a8c1a6151484eac9
sobhika11 <sobhika1105@gmail.com>
2025-12-22 07:45:20 +0530

    createtest function

---
+++ backend/controllers/test.controller.js
@@ -0,0 +1,9 @@
+const Test = require("../models/Test");
+
+exports.createTest = async (req, res) => {
+  try {
+    return res.status(200).json({ message: "createTest controller" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
