b12f8dae306be6643e872054fd4041bfc0cb98de
sobhika11 <sobhika1105@gmail.com>
2025-12-22 07:58:20 +0530

    validation of test registering process

--- backend/controllers/test.controller.js
+++ backend/controllers/test.controller.js
@@ -45,22 +45,29 @@ const testRegister = async (req, res) => {
     }
 
     const test = await Test.findOne({ test_code });
-
     if (!test) {
       return res.status(404).json({ msg: "Invalid test code" });
     }
 
-    res.status(200).json({ msg: "Test code is valid" });
+    const user = await User.findById(userId);
+
+    if (user.test_code === test_code) {
+      return res.status(400).json({ msg: "You are already registered for this test" });
+    }
+
+    // Register student
+    user.test_code = test_code;
+    await user.save();
+
+    res.status(200).json({ msg: "Successfully registered for the test" });
 
   } catch (error) {
     res.status(500).json({
-      msg: "Error while validating test code",
+      msg: "Error while registering for test",
       error: error.message
     });
   }
 };
-
-
 exports.testAdminData = async (req, res) => {
   try {
     return res.status(200).json({ message: "testAdminData controller" });
