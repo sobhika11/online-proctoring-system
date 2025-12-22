13d2abd4bf7bce0b6d90408dd882add574845735
sobhika11 <sobhika1105@gmail.com>
2025-12-22 07:50:20 +0530

    completed controller

--- backend/controllers/test.controller.js
+++ backend/controllers/test.controller.js
@@ -7,9 +7,26 @@ exports.createTest = async (req, res) => {
     return res.status(500).json({ error: err.message });
   }
 };
+
 exports.userCreatedTests = async (req, res) => {
   try {
     return res.status(200).json({ message: "userCreatedTests controller" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+exports.testRegister = async (req, res) => {
+  try {
+    return res.status(200).json({ message: "testRegister controller" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+exports.testAdminData = async (req, res) => {
+  try {
+    return res.status(200).json({ message: "testAdminData controller" });
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
