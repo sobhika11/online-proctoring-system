23401f26879a72ed48280d3bdde7159264f67a5a
sobhika11 <sobhika1105@gmail.com>
2026-01-02 15:24:48 +0530

    Backend server testing done

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,34 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
-
+const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
-console.log(process.env.MONGO_URI);
+
+const testRoutes = require("./routes/testRoutes");
+const attemptRoutes = require("./routes/attemptRoutes");
+const app = express();
+
+app.use(express.json());
+app.use("/api/tests", testRoutes);
+app.use("/api/attempts", attemptRoutes);=
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Server Error",
+  });
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server running");
+    });
+  })
+  .catch((err) => console.log(err));
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+console.log("Your backend is running in http://localhost:2000");
+
