ee592909b5c3b2b7ec0032201c2538520e4ec353
sobhika11 <sobhika1105@gmail.com>
2026-01-04 23:11:34 +0530

    Completed server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const app = express();
 
 const testRoutes = require("./routes/testRoutes");
 const attemptRoutes = require("./routes/attemptRoutes");
-const app = express();
 const authRoutes = require("./routes/authRoutes");
 
-app.use("/api/auth", authRoutes);
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 app.use("/api/tests", testRoutes);
 app.use("/api/attempts", attemptRoutes);
 
@@ -24,7 +25,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT || 2000, () => {
+    app.listen(process.env.PORT || 2001, () => {
       console.log("Server running");
     });
   })
@@ -32,5 +33,5 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
-console.log("Your backend is running in http://localhost:2000");
+console.log("Your backend is running in http://localhost:2001");
 
