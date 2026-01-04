cbf164f1bd17c028f5c8920691845bc123850ea5
sobhika11 <sobhika1105@gmail.com>
2026-01-04 23:12:58 +0530

    Validation done for testAttempt

--- backend/routes/attemptRoutes.js
+++ backend/routes/attemptRoutes.js
@@ -7,15 +7,18 @@ const {
 } = require("../controllers/testAttempt");
 
 const { authRole } = require("../middleware/validate");
+const authenticate = require("../middleware/auth");
 
 router.post(
   "/start/:test_code",
+  authenticate,
   authRole(["student"]),
   startTest
 );
 
 router.post(
   "/submit/:test_code",
+  authenticate,
   authRole(["student"]),
   submitTest
 );
