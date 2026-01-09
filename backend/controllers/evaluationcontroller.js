c4cf276b5c780998875ab3ff540b1ed2434f1f26
sobhika11 <sobhika1105@gmail.com>
2026-01-09 11:29:16 +0530

    evaluating question logic

--- backend/controllers/evaluationcontroller.js
+++ backend/controllers/evaluationcontroller.js
@@ -36,4 +36,33 @@ exports.evaluateAttempt = async (attemptId) => {
     // Calculate max marks
     questions.forEach(q => {
       maxMarks += q.marks || 1;
+    });    
+    // Evaluate each question
+    const evaluationDetails = questions.map(question => {
+      const answer = answers.find(a => a.questionId.toString() === question._id.toString());
+      if (!answer) {
+        unansweredCount++;
+        return {
+          questionId: question._id,
+          status: "unanswered",
+          selectedOption: null,
+          correctOption: question.correctOption,
+          marks: 0
+        };
+      }
+      const isCorrect = answer.selectedOption === question.correctOption;
+      const marks = isCorrect ? (question.marks || 1) : 0;
+      if (isCorrect) {
+        correctCount++;
+        totalScore += marks;
+      } else {
+        wrongCount++;
+      }
+      return {
+        questionId: question._id,
+        status: isCorrect ? "correct" : "wrong",
+        selectedOption: answer.selectedOption,
+        correctOption: question.correctOption,
+        marks
+      };
     });
